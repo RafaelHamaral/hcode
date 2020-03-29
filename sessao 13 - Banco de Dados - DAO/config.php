e2aa292cc3261a14872adfd828eea55fa27aa93a
RafaelHamaral <rafael_holandamaral@hotmail.com>
2020-03-29 12:27:32 -0300

    sessao 13 - criacao da classe usuario

--- sessao 13 - Banco de Dados - DAO/config.php
+++ sessao 13 - Banco de Dados - DAO/config.php
@@ -4,15 +4,13 @@
 
 spl_autoload_register(function($class_name){
 
-    $filename = $class_name. ".php";
+    $filename ="class". DIRECTORY_SEPARATOR. $class_name. ".php"; //uso assim porque criei umas pasta para inseir as classes
 
     if (file_exists(($filename))){
 
         require_once($filename); //se o arquivo ja existir faz o require dele
 
     }
-
-
 
 });
 
--- sessao 13 - Banco de Dados - DAO/index.php
+++ sessao 13 - Banco de Dados - DAO/index.php
@@ -2,13 +2,22 @@
 
 // oque sera executado (primeira pagina no localhost)
 
-require_once("config.php");
+/*require_once("config.php"); 
 
 $sql = new Sql();//encontrar a classe sql
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");//se encontrar Sql vai executar esse metodos
 
 
-echo json_encode($usuarios);
+echo json_encode($usuarios); //vai gerar todos os dados do banco em json*/
+
+require_once("config.php");
+
+$root = new Usuario();
+
+$root->loadById(6);
+
+echo $root;
+
 
 ?>
